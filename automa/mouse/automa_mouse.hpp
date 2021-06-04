7fbb9af63f74ef5c1461a7133fc2399563fd2e0b
Gabriele DAnesio <gabriele.danesio@studio.unibo.it>
2021-06-04 18:51:28 +0200

    Implementazione viaggi tra porti e tra aeroporti

--- automa/mouse/automa_mouse.hpp
+++ automa/mouse/automa_mouse.hpp
@@ -1,5 +1,6 @@
 //g++ main_a.cpp -Wall -Wextra -lsfml-graphics -lsfml-system -lsfml-window
 #include <vector>
+#include <array>
 #include <cassert>
 #include <random>
 #include <cmath>
@@ -23,6 +24,10 @@ enum class Condition : char
 struct from_to
 {
   int r_f, c_f, r_t, c_t;
+};
+struct point
+{
+  int r, c;
 };
 
 struct State
@@ -183,6 +188,49 @@ inline bool probability(double prob)
   return exitus <= prob;
 }
 
+inline void move_port(World &current, Condition porto, int oldR, int oldC)
+{
+  std::vector<point> destCell;
+  destCell.resize(200);
+  int i = 0;
+  for (int r = 0; r < current.getNRighe(); ++r)
+  {
+    for (int c = 0; c < current.getNCol(); ++c)
+    {
+      if (current.getCondition(r, c) == porto)
+      {
+        if (neighbours<Condition::Empty>(current, r, c) != 0)
+        {
+          int row = -1, column;
+          for (; row != 2; ++row)
+          {
+            for (column = -1; column != 2; ++column)
+            {
+              if (current.getCondition(r + row, c + column) == Condition::Empty)
+              {
+                destCell.push_back(point{r + row, c + column});
+                ++i;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  if (i == 0)
+    return;
+  std::default_random_engine gen{std::random_device{}()};
+  std::uniform_int_distribution<short int> dist(0, destCell.size() - 1);
+
+  short int arriveCell = dist(gen);
+  //std::cerr << "i: " << i << "\tr: " << destCell[arriveCell].r <<"\tc: " << destCell[arriveCell].c << "\n\n\n";
+  if(current.getCondition(destCell[arriveCell].r, destCell[arriveCell].c) == porto || current.getCondition(oldR, oldC) == porto)
+    return;
+  Condition temp = current.getCondition(destCell[arriveCell].r, destCell[arriveCell].c);
+  current.setCondition(destCell[arriveCell].r, destCell[arriveCell].c) = current.getCondition(oldR, oldC);
+  current.setCondition(oldR, oldC) = temp;
+}
+
 inline void move_cell(World &current, int day)
 {
   std::vector<from_to> fromTo;
@@ -203,6 +251,8 @@ inline void move_cell(World &current, int day)
     for (int c = start_col; c != n_col; c += each)
     {
       short int empty_cells = neighbours<Condition::Empty>(current, r, c);
+      empty_cells += neighbours<Condition::Airport>(current, r, c);
+      empty_cells += neighbours<Condition::Port>(current, r, c);
       if (empty_cells == 0 || current.getCondition(r, c) == Condition::Empty || current.getCondition(r, c) == Condition::Dead)
       {
         continue;
@@ -219,7 +269,7 @@ inline void move_cell(World &current, int day)
       {
         for (column = -1; column != 2 && count_empty != exitus; ++column)
         {
-          if (current.getCondition(r + row, c + column) == Condition::Empty)
+          if (current.getCondition(r + row, c + column) == Condition::Empty || current.getCondition(r + row, c + column) == Condition::Port || current.getCondition(r + row, c + column) == Condition::Airport)
           {
             ++count_empty;
           }
@@ -228,6 +278,11 @@ inline void move_cell(World &current, int day)
         }
         if (count_empty == exitus)
           break;
+      }
+      if (current.getCondition(r + row, c + column) == Condition::Port || current.getCondition(r + row, c + column) == Condition::Airport)
+      {
+        move_port(current, current.getCondition(r + row, c + column), r, c);
+        continue;
       }
 
       current.setCondition(r + row, c + column) = Condition::Booked;
@@ -293,10 +348,10 @@ class Graphics
   float pan_x, pan_y, width_p, height_p, pixel_ratio;
 
 public:
-  Graphics(sf::RenderWindow &win_in, sf::Image& image_in, short imm_width_in, short imm_height_in) : window{win_in}, image{image_in}, imm_width{imm_width_in}, imm_height{imm_height_in}
+  Graphics(sf::RenderWindow &win_in, sf::Image &image_in, short imm_width_in, short imm_height_in) : window{win_in}, image{image_in}, imm_width{imm_width_in}, imm_height{imm_height_in}
   {
     mappa.resize(imm_height * imm_width * 8);
-    pan_x = 0.025 * width, pan_y = 0.025 * height;
+    pan_x = 0.025 * width, pan_y = 0.05 * height;
     width_p = .95 * width / imm_width, height_p = .95 * height / imm_height;
     pixel_ratio = width_p;
     if (width_p > height_p)
@@ -340,10 +395,10 @@ public:
       {
         c += 4;
         int index{r * imm_width * 4 + c};
-        mappa[index].position = sf::Vector2f(col * 2 * lato + pan_x,                      row * 2 * lato + pan_y);
-        mappa[index + 1].position = sf::Vector2f(col * 2 * lato + pan_x,                  row * 2 * lato + pan_y + spaziatura);
-        mappa[index + 2].position = sf::Vector2f(col * 2 * lato + pan_x + spaziatura,     row * 2 * lato + pan_y + spaziatura);
-        mappa[index + 3].position = sf::Vector2f(col * 2 * lato + pan_x + spaziatura,     row * 2 * lato + pan_y);
+        mappa[index].position = sf::Vector2f(col * 2 * lato + pan_x, row * 2 * lato + pan_y);
+        mappa[index + 1].position = sf::Vector2f(col * 2 * lato + pan_x, row * 2 * lato + pan_y + spaziatura);
+        mappa[index + 2].position = sf::Vector2f(col * 2 * lato + pan_x + spaziatura, row * 2 * lato + pan_y + spaziatura);
+        mappa[index + 3].position = sf::Vector2f(col * 2 * lato + pan_x + spaziatura, row * 2 * lato + pan_y);
 
         mappa[index].color = sf::Color::Black;
         mappa[index + 1].color = sf::Color::Black;
@@ -352,58 +407,58 @@ public:
 
         switch (pan.getCondition(row, col))
         {
-          case Condition::Susceptible:
-          {
-            mappa[index].color = sf::Color::Blue;
-            mappa[index + 1].color = sf::Color::Blue;
-            mappa[index + 2].color = sf::Color::Blue;
-            mappa[index + 3].color = sf::Color::Blue;
-            break;
-          }
-          case Condition::Infected:
-          {
-            mappa[index].color = sf::Color::Red;
-            mappa[index + 1].color = sf::Color::Red;
-            mappa[index + 2].color = sf::Color::Red;
-            mappa[index + 3].color = sf::Color::Red;
-            break;
-          }
-          case Condition::Healed:
-          {
-            mappa[index].color = sf::Color::Green;
-            mappa[index + 1].color = sf::Color::Green;
-            mappa[index + 2].color = sf::Color::Green;
-            mappa[index + 3].color = sf::Color::Green;
-            break;
-          }
-          case Condition::Dead:
-          {
-            mappa[index].color = sf::Color{220, 200, 0, 255};
-            mappa[index + 1].color = sf::Color{220, 200, 0, 255};
-            mappa[index + 2].color = sf::Color{220, 200, 0, 255};
-            mappa[index + 3].color = sf::Color{220, 200, 0, 255};
-            break;
-          }
-          case Condition::Port:
-          {
-            mappa[index].color = sf::Color::Magenta;
-            mappa[index + 1].color = sf::Color::Magenta;
-            mappa[index + 2].color = sf::Color::Magenta;
-            mappa[index + 3].color = sf::Color::Magenta;
-            break;
-          }
-          case Condition::Airport:
-          {
-            mappa[index].color = sf::Color::Cyan;
-            mappa[index + 1].color = sf::Color::Cyan;
-            mappa[index + 2].color = sf::Color::Cyan;
-            mappa[index + 3].color = sf::Color::Cyan;
-            break;
-          }
-          default:
-          {
-            break;
-          }
+        case Condition::Susceptible:
+        {
+          mappa[index].color = sf::Color::Blue;
+          mappa[index + 1].color = sf::Color::Blue;
+          mappa[index + 2].color = sf::Color::Blue;
+          mappa[index + 3].color = sf::Color::Blue;
+          break;
+        }
+        case Condition::Infected:
+        {
+          mappa[index].color = sf::Color::Red;
+          mappa[index + 1].color = sf::Color::Red;
+          mappa[index + 2].color = sf::Color::Red;
+          mappa[index + 3].color = sf::Color::Red;
+          break;
+        }
+        case Condition::Healed:
+        {
+          mappa[index].color = sf::Color::Green;
+          mappa[index + 1].color = sf::Color::Green;
+          mappa[index + 2].color = sf::Color::Green;
+          mappa[index + 3].color = sf::Color::Green;
+          break;
+        }
+        case Condition::Dead:
+        {
+          mappa[index].color = sf::Color{220, 200, 0, 255};
+          mappa[index + 1].color = sf::Color{220, 200, 0, 255};
+          mappa[index + 2].color = sf::Color{220, 200, 0, 255};
+          mappa[index + 3].color = sf::Color{220, 200, 0, 255};
+          break;
+        }
+        case Condition::Port:
+        {
+          mappa[index].color = sf::Color{185, 185, 185, 255};
+          mappa[index + 1].color = sf::Color{185, 185, 185, 255};
+          mappa[index + 2].color = sf::Color{185, 185, 185, 255};
+          mappa[index + 3].color = sf::Color{185, 185, 185, 255};
+          break;
+        }
+        case Condition::Airport:
+        {
+          mappa[index].color = sf::Color::White;
+          mappa[index + 1].color = sf::Color::White;
+          mappa[index + 2].color = sf::Color::White;
+          mappa[index + 3].color = sf::Color::White;
+          break;
+        }
+        default:
+        {
+          break;
+        }
         }
       }
       ++r;
@@ -415,23 +470,22 @@ public:
     window.draw(mappa);
     window.display();
   }
+  inline void WriteText(const std::string &string, short pos_x, short pos_y)
+  {
+    sf::Font font;
+    if (!font.loadFromFile("arial.ttf"))
+      std::cerr << "\n\nError loading font\n\n";
+    sf::Text text;
+    text.setFont(font);
+    text.setCharacterSize(20);
+    text.setFillColor(sf::Color::White);
+    text.setString(string);
+    float text_w = text.getGlobalBounds().width;
+    float text_h = text.getGlobalBounds().height;
+    text.setPosition(pos_x - text_w / 2, (pos_y - text_h));
+    window.draw(text);
+  }
 };
-
-inline void WriteText(sf::RenderWindow &window, const std::string &string, short pos_x, short pos_y)
-{
-  sf::Font font;
-  if (!font.loadFromFile("arial.ttf"))
-    std::cerr << "\n\nError loading font\n\n";
-  sf::Text text;
-  text.setFont(font);
-  text.setCharacterSize(pos_y / 2);
-  text.setFillColor(sf::Color::White);
-  text.setString(string);
-  float text_w = text.getGlobalBounds().width;
-  float text_h = text.getGlobalBounds().height;
-  text.setPosition(pos_x - text_w / 2, (pos_y - text_h) / 2);
-  window.draw(text);
-}
 
 /*void changeState(sf::RenderWindow& window, World& current)
 {
@@ -455,7 +509,8 @@ void Window(int T, World &pan, sf::Image image, short width = 800, short height 
   window.draw(background);
 
   Graphics graph(window, image, image.getSize().x, image.getSize().y);
-
+  graph.createArray(pan);
+  graph.drawArray();
   sf::Vector2u dim = image.getSize();
   short imm_height = dim.x, imm_width = dim.y;
   const float pan_x = 0.025 * width, pan_y = 0.025 * height;
@@ -464,7 +519,7 @@ void Window(int T, World &pan, sf::Image image, short width = 800, short height 
   if (width_p > height_p)
     pixel_ratio = height_p;
   graph.loadImage(pan);
-  
+
   while (!sf::Mouse::isButtonPressed(sf::Mouse::Button::Right)) //Scelta da mouse
   {
     while (!sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
@@ -474,7 +529,7 @@ void Window(int T, World &pan, sf::Image image, short width = 800, short height 
     int pos_y = std::round((pos.y) / 2.);
     std::cerr << "x: " << pos.x << "    col: " << pos_x << "\ty: " << pos.y << "   row:" << pos_y << "\n\n";
     //if (pos_x >= imm_width || pos_x < 0 || pos_y >= imm_height || pos_y < 0)
-      //continue;
+    //continue;
     Condition state = pan.getCondition(pos_y, pos_x);
     if (state != Condition::Susceptible && state != Condition::Infected && state != Condition::Empty)
       continue;
@@ -514,8 +569,8 @@ void Window(int T, World &pan, sf::Image image, short width = 800, short height 
     }
     std::string dati{"Day: "};
     dati = dati + std::to_string(i) + "   Susceptibles: " + std::to_string(suscep) + "   Infected: " + std::to_string(infected) + "   Healed: " + std::to_string(healed) + "   Dead: " + std::to_string(deaths);
-    WriteText(window, dati, width / 2, pan_y);
-
+    graph.WriteText(dati, width / 2, height - pan_y);
+    window.display();
     //Vaccinazioni
     std::default_random_engine eng{std::random_device{}()};
     std::uniform_int_distribution<int> dist_righe{0, imm_height - 1}, dist_col{0, imm_width - 1};
