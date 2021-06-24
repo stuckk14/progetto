e2c63018e9d4ddfdfebc218dea4770138c1d0900
Gabriele DAnesio <gabriele.danesio@studio.unibo.it>
2021-06-24 20:23:33 +0200

    Aggiunta TU grafica (parte 1)

---
+++ Parte1/graphics1.cpp
@@ -0,0 +1,86 @@
+#include "graphics1.hpp"
+void Graphics::drawAxis()
+{
+    sf::RectangleShape line(sf::Vector2f(width * 0.8, 3));
+    line.setFillColor(sf::Color::Black);
+    line.setPosition(pan_x, pan_y);
+    window.draw(line);
+    line.scale((float)height / width, 1);
+    line.rotate(270);
+    window.draw(line);
+}
+
+void Graphics::WriteTextGraph(const std::string &string, short pos_x, short pos_y)
+{
+    sf::Font font;
+    if (!font.loadFromFile("../arial.ttf"))
+        std::cerr << "\n\nError loading font\n\n";
+    sf::Text text;
+    text.setFont(font);
+    text.setCharacterSize(15);
+    text.setFillColor(sf::Color::Black);
+    text.setString(string);
+    float text_w = text.getGlobalBounds().width;
+    text.setPosition(pos_x - text_w, pos_y);
+    window.draw(text);
+}
+void Graphics::drawGraph(Population &state, const int i, const int duration)
+{
+    sf::CircleShape point(3.f);
+    const int N = std::round(state.S + state.I + state.R);
+    const float y_s = state.S / N * (height * 0.8);
+    const float y_i = state.I / N * (height * 0.8);
+    const float y_r = state.R / N * (height * 0.8);
+    point.setPosition(pan_x + i * (width * .8f / duration), -y_s + pan_y);
+    point.setFillColor(sf::Color::Blue);
+    window.draw(point);
+    point.setPosition(pan_x + i * (width * .8f / duration), -y_i + pan_y);
+    point.setFillColor(sf::Color::Red);
+    window.draw(point);
+    point.setPosition(pan_x + i * (width * .8f / duration), -y_r + pan_y);
+    point.setFillColor(sf::Color{0, 200, 0, 255});
+    window.draw(point);
+}
+void printSIR(Population &state, const int day)
+{
+  const int N = std::round(state.S + state.I + state.R);
+  std::cout << day << '.' << std::setw(18 - static_cast<int>(log10(day))) << std::round(state.S);
+  std::cout << std::setw(19);
+  std::cout << std::round(state.I);
+  std::cout << std::setw(19);
+  //std::cout << std::round(state.R) << std::setw(20) << std::round(state.S + state.I + state.R) << '\n';
+  std::cout << N - std::round(state.S) - std::round(state.I) << '\n';
+  std::cout << "-----------------------------------------------------------------------------------------------" << '\n';
+}
+
+void window(int T, Pandemy &pan, int N, short width = 800, short height = 600)
+{
+  sf::RenderWindow window(sf::VideoMode(width, height), "Andamento S, I e R");
+  //sf::RectangleShape point(sf::Vector2f(3.f, 3.f));
+  window.clear(sf::Color::White);
+
+  sf::RectangleShape line(sf::Vector2f(width * 0.8, 3));
+  line.setFillColor(sf::Color::Black);
+  const float pan_x = 0.1 * width, pan_y = 0.9 * height;
+  Graphics graph{window, width, height, pan_x, pan_y};
+  graph.drawAxis();
+  Population state;
+  for (int i = 0; i < T; ++i)
+  {
+    pan.evolve();
+    state = pan.GetState();
+    assert(std::round(state.S + state.I + state.R) == N);
+    graph.drawGraph(state, i, T);
+    printSIR(state, i + 1);
+  }
+
+  graph.WriteTextGraph(std::to_string(N), pan_x - 3.f, height - pan_y); //Y max
+  graph.WriteTextGraph(std::to_string(T), width - pan_x, pan_y);        //X max
+  graph.WriteTextGraph(std::to_string(N / 2), pan_x - 3.f, height / 2); //Y max / 2
+  graph.WriteTextGraph(std::to_string(T / 2), width / 2, pan_y);        //Y max / 2
+  window.display();
+  sf::Event chiusura;
+  while (window.waitEvent(chiusura))
+    if (chiusura.type == sf::Event::Closed)
+      window.close();
+}
---
+++ Parte1/graphics1.hpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <cmath>
+#include <cassert>
+#include <iostream>
+#include <thread>
+#include <iomanip>
+#include <SFML/Graphics.hpp>
+#include "pandemy1.hpp"
+#ifndef GRAPHICS1_HPP
+#define GRAPHICS1_HPP
+struct SIR
+{
+  int S{}, I{}, H{}, D{};
+};
+class Graphics
+{
+  sf::RenderWindow &window;
+  short width, height;
+  float pan_x, pan_y;
+
+public:
+  Graphics(sf::RenderWindow &win_in, short width_in, short height_in, float pan_x_in, float pan_y_in) : window{win_in}, width{width_in}, height{height_in}, pan_x{pan_x_in}, pan_y{pan_y_in} {}
+
+  void drawAxis();
+  void WriteTextGraph(const std::string &string, short pos_x, short pos_y);
+  void drawGraph(Population &state, const int i, const int duration);
+};
+
+
+void printSIR(Population &);
+
+void window(int, Pandemy &, int, short, short);
+#endif
